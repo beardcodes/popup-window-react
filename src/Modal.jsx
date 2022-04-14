1e0ceb0b9302b0ca02e89f3fbf2b7a1f7d1ca9c6
molessom-8 <elquaraoui@gmail.com>
2022-04-14 10:39:02 +0800

    first commit

---
+++ src/Modal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import nft from'./nft.jpg';
+
+const Modal = ({ open,onClose }) => {
+    if(!open) return null
+  return (
+    <div onClick={onClose} className="overlay">
+      <div onClick={(e) => {
+          e.stopPropagation()
+      }} className="modalContainer">
+        <img src={nft} atl= '' />
+        <div className="modalRight">
+            <p onClick={onClose} className="closeBtn">X</p>
+            <div className="content">
+            <p>Do you want a</p>
+            <h1>$20 CREDIT</h1>
+            <p>for your first trade?</p>
+            </div>
+            <div className="btnContainer">
+            <button className="btnPrimary">
+              <span className="bold">Yes</span>, I love NFT's
+            </button>
+            <button className="btnOutline">
+              <span className="bold">NO</span>, thanks
+            </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
